1e2db45ba3733c41b09c633ec891bf092c7782a1
Crackexy <67500032+Crackexy@users.noreply.github.com>
2020-08-28 11:09:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # Your Bot token in the quotes taken from @BotFather
-BOT_TOKEN = "1158822364:AAEgnhKPvoTbPU5KnO8iMYqQfuF_XcxbBUE"
+BOT_TOKEN = "1185486427:AAFjikqt6kJu8GlPYjYRQLKm6lEfVq4I9bI"
 
 # Project Name of your Project
-PROJECT_NAME = "GitGram"
+PROJECT_NAME = "CrackBot"
 
 # If you forked the source code, grab your repo's link and paste it below!
-GIT_REPO_URL = "https://github.com/MadeByThePinsHub/GitGram"
+GIT_REPO_URL = "https://github.com/Crackexy/CrackBot"
